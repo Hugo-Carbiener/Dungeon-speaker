9e13e7a77980fa4ecad9a000080ec4bd3db23a13
Hugo's Laptop <hugocarbiener@gmail.com>
2021-03-16 18:16:41 +0100

    Add moveTo and moveForward methods

--- game/src/main/Main.java
+++ game/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 
 import dungeon.Map;
 import dungeon.Room;
+import player.Hero;
 
 public class Main {
 
@@ -11,30 +12,35 @@ public class Main {
 		while (map.getRoom("011", map.getStartingRoom()) == null) {
 			map = new Map(4,4,1);
 		}
+		Hero hero = new Hero("Kohuro", map);
+		System.out.println(hero.getPosition().getId());
+		
+		System.out.println(map.getRoom("0", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("00", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("01", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("010", map.getStartingRoom()).wasVisited());
+		
+		hero.moveTo(map.getRoom("01", map.getStartingRoom()));
+		System.out.println(hero.getPosition().getId());
+		
+		System.out.println(map.getRoom("0", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("00", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("01", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("010", map.getStartingRoom()).wasVisited());
+		
+		hero.moveForward();
+		System.out.println(hero.getPosition().getId());
+		
+		System.out.println(map.getRoom("0", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("00", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("01", map.getStartingRoom()).wasVisited());
+		System.out.println(map.getRoom("010", map.getStartingRoom()).wasVisited());
+		
 		Room root = map.getStartingRoom();
 		
 		map.displayFromRoom(root);		
-		System.out.println();System.out.println();
 		
-		Room testRoom = map.getRoom("011", root);
-		System.out.println("Test room 011");
-		System.out.println();
-		System.out.print("Previous room is : ");
-		System.out.println(testRoom.getPreviousRoom().getId());
-		System.out.println();
-		System.out.println("Next rooms are : ");
-		for (Room each : testRoom.getNextRooms()) {
-			System.out.println(each.getId());
-		}
-		System.out.println();
-		System.out.println("Accessible rooms are : ");
-		for (Room each : testRoom.getAccessibleRooms()) {
-			System.out.println(each.getId());
-		}
-		System.out.println();
-		System.out.println("Room has neighbors ?");
-		System.out.println(testRoom.getNeighbor(map));
-				
+		
 	}
 }
 	
--- game/src/player/Hero.java
+++ game/src/player/Hero.java
@@ -1,22 +1,29 @@
 package player;
 
+import dungeon.Map;
+import dungeon.Room;
+
 public class Hero extends Character{
-	String username;
-	//Room position;
-	int level;
+	private String username;
+	private Room position;
+	private int level;
 	int xp;
 	//int xpCap;
 	
-	public Hero(String username) {
+	public Hero(String username, Map map) {
 		//Default settings for a new player
 		super(20, 20, 1);
 		this.username = username;
+		this.position = map.getStartingRoom();
 		xp = 0;
 		//xpCap = getxpCap(int level);
+		//Hero appears in the starting room so it is visited
+		this.position.setAsVisited();
 	}
 	
 	public String getUsername() {return this.username;}
 	public int getLevel() {return this.level;}
+	public Room getPosition() {return this.position;}
 	
 	public void equipItem(Item item) {
 		if (item.isEquipable()|| item.isThrowable()) {
@@ -50,6 +57,23 @@ public class Hero extends Character{
 		
 	}
 	
+	public void moveTo(Room room) {
+		//Move player to the chosen room if it is accessible
+		Room currentRoom = this.position;
+		if (currentRoom.getAccessibleRooms().contains(room)) {
+			this.position = room;
+			room.setAsVisited();
+		} else {
+			System.out.println("Room is not accessible");
+		}
+	}
+	
+	public void moveForward() {
+		//Move the hero to the first child room
+		Room currentRoom = this.position;
+		Room nextRoom = currentRoom.getNextRooms().get(0);
+		this.moveTo(nextRoom);
+	}
 	
 	
 }
