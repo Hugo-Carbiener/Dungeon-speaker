6b248f03d35892138dd67408a2da043ef2f0c991
Hugo's Laptop <hugocarbiener@gmail.com>
2021-03-17 20:52:26 +0100

    Add lib javatuples, add monsterGeneration draft

--- game/src/main/Main.java
+++ game/src/main/Main.java
@@ -8,9 +8,9 @@ import player.Hero;
 public class Main {
 	
 	public static void main(String[] args) {
-		Map map = new Map(4,4,1);
+		Map map = new Map(4,4,0.25);
 		while (map.getRoom("011", map.getStartingRoom()) == null) {
-			map = new Map(4,4,1);
+			map = new Map(4,4,0.25);
 		}
 		Room root = map.getStartingRoom();
 		Hero hero = new Hero("Kohuro", map);
--- game/src/player/Monster.java
+++ game/src/player/Monster.java
@@ -1,23 +1,38 @@
 package player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.javatuples.Pair;
+
 
 public class Monster extends Character{
 
 	private String name;
+	private boolean isAlive = true;		//Is used when entering a room : if the monster is alive then a combat has to start  with the player
 	
 
 	
 	public Monster(String name) {
 		super(10, 0, 0.5, 2);
+		
 		this.name = name;
 		
 		
 	}
 	
-	public String getName() {
-		return this.name;
+	public String getName() {return this.name;}
+	
+	public void monsterGeneration() {
+		Pair<String, Integer> pair = Pair.with("blabla", 1);
+		List<Pair<String, Integer>> monsters = new ArrayList<>(Arrays.asList(
+		
+		("dragon", 10)
+				));
+			
+		};
+		
 	}
-	
-	
 
 }
