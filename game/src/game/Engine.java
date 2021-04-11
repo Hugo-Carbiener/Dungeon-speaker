a5bb8664342a19c1dd3cf8e187d5e0cd47738170
Hugo's Laptop <hugocarbiener@gmail.com>
2021-04-11 19:39:01 +0200

    Add map/player generation in the engine, define GameState

--- game/src/game/Engine.java
+++ game/src/game/Engine.java
@@ -1,5 +1,27 @@
 package game;
 
-public class Engine {
+import dungeon.Map;
+import player.Hero;
 
+public class Engine {
+	
+	public static void init() {
+		//init gamestate
+		GameState gameState = GameState.init();
+		
+		//init GUI with title screen 
+		
+		//after pressing "start" button
+		//prompt playerName
+		String playerName = "";
+		
+		//GENERATE MAP
+		Map map = Map.generateMap(4,4,0.5, 0.5, 20, 30);
+		//GENERATE PLAYER 
+		Hero player = new Hero(playerName, map);
+		//Set GameState to playing phase
+		gameState.setGamestate("Roaming");
+		
+		
+	}
 }
--- game/src/game/GameState.java
+++ game/src/game/GameState.java
@@ -1,5 +1,16 @@
 package game;
 
 public class GameState {
-
+	
+	private String gamestate;
+	
+	
+	public static GameState init() {
+		GameState gameState = new GameState();
+		gameState.gamestate = "TitleScreen";
+		return gameState;
+	}
+	
+	public String getGameState() {return this.gamestate;}
+	public void setGamestate(String gamestate) {this.gamestate = gamestate;}
 }
