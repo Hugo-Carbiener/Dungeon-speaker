3f566431f3e4fc09b4ee04be6a4377ab2b24f2c6
Hugo's Laptop <hugocarbiener@gmail.com>
2021-04-18 23:06:27 +0200

    Implement GUI to project

---
+++ game/src/gui/GUI2.java
@@ -0,0 +1,58 @@
+package gui;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
+public class GUI2 implements ActionListener {
+	public GUI2() {
+		 //Creating the Frame
+        JFrame frame = new JFrame("Dungeon Speaker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(40); // accepts upto 40 characters
+        JButton send = new JButton("Send");
+        send.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String str = tf.getText();
+				System.out.println(str);
+				
+			}
+        	
+        });
+        JButton reset = new JButton("Reset");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea("Bienvenue " +GUI3.username);
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+	}
+
+}
--- game/src/main/Main.java
+++ game/src/main/Main.java
@@ -9,38 +9,14 @@ import player.Hero;
 import player.Item;
 import player.Monster;
 import player.Weapon;
+import gui.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Map map = new Map(4,4,0.25,0.5);
-		while (map.getRoom("011", map.getStartingRoom()) == null) {
-			map = new Map(4,4,0.25,0.5);
-		}
-		Room root = map.getStartingRoom();
-		Hero hero = new Hero("Kohuro", map);
-
-		map.displayFromRoom(root);
+		System.out.println("je suis au bord du suicide");
 		
-		hero.moveForward();
-		if (hero.getPosition().getItems().size()!=0) {
-		hero.take(hero.getPosition().getItems().get(0));
-		}
-		hero.moveBackwards();
-		hero.moveTo(map.getRoom("01", root));
-		if (hero.getPosition().getItems().size()!=0) {
-			hero.take(hero.getPosition().getItems().get(0));
-			}
-		hero.moveForward();
-		if (hero.getPosition().getItems().size()!=0) {
-			hero.take(hero.getPosition().getItems().get(0));
-			}
-		List<Item> inventory = hero.inventory;
-		for (Item each : inventory) {
-			System.out.println(((Weapon) each).getName());
-		}
-		
-		
+		new GUI();
 	}
 }
 	
