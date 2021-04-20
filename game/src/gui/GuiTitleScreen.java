67a91a7b98c391f7b82f0c1a24c9347cfcb0d8a9
Hugo's Laptop <hugocarbiener@gmail.com>
2021-04-20 10:41:26 +0200

    Fix librairies issues

--- game/src/gui/GuiTitleScreen.java
+++ game/src/gui/GuiTitleScreen.java
@@ -9,7 +9,6 @@ public class GuiTitleScreen implements ActionListener {
 
 	public GuiTitleScreen() {
 		JFrame frame = new JFrame();
-		frame.setSize(1920, 1080);
 		JButton button = new JButton("Start");
 		button.addActionListener(new ActionListener() {
 
@@ -26,7 +25,7 @@ public class GuiTitleScreen implements ActionListener {
 		panel.add(button);
 		
 		frame.add(panel,BorderLayout.CENTER);
-        frame.setSize(400, 400);
+        //frame.setSize(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Dungeon Speaker");
 		frame.pack();
