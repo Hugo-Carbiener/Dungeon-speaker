dcac20b88bffb2fab3ebcdf49ede3ad34713e080
Hugo's Laptop <hugocarbiener@gmail.com>
2021-05-07 12:43:30 +0200

    Modify style in game window

--- game/src/gui/GuiGameWindow.java
+++ game/src/gui/GuiGameWindow.java
@@ -2,13 +2,16 @@ package gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 
 import javax.swing.*;
 
 
 public class GuiGameWindow implements ActionListener {
+	
 	private final static String newline = "\n";
+	
 	public GuiGameWindow() throws FontFormatException, IOException {
 		 //Creating the Frame
         JFrame frame = new JFrame("Dungeon Speaker");
@@ -17,15 +20,13 @@ public class GuiGameWindow implements ActionListener {
 
         //Creating the panel at bottom and adding components
         JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
+        JLabel label = new JLabel("Describe your action :  ");        
         JTextField tf = new JTextField(40); // accepts upto 40 characters
         // Text Area at the Center
         JTextArea ta = new JTextArea("Bienvenue " +GuiGameMenu.username);
         ta.setEditable(false);
-         
-        JScrollPane scroll = new JScrollPane (ta);
         
-
+        JScrollPane scroll = new JScrollPane (ta);
         
         //JButton send = new JButton("Send");
         JButton send = Toolkit.simpleButton("Send", "pixelArtFont.ttf");
@@ -45,6 +46,14 @@ public class GuiGameWindow implements ActionListener {
         panel.add(tf);
         panel.add(send);
         panel.add(scroll);
+        
+        //Style
+        Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/res/pixelArtFont.ttf")).deriveFont(40f);
+        label.setFont(customFont);
+        label.setForeground(Color.WHITE);
+        ta.setBackground(Color.BLACK);
+        ta.setForeground(Color.WHITE);
+        panel.setBackground(Color.BLACK);
         
         frame.getRootPane().setDefaultButton(send);
     
