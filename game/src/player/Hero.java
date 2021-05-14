579e4d71d40eb630af6926361a79b600d635f307
Hugo's Laptop <hugocarbiener@gmail.com>
2021-05-14 15:51:29 +0200

    Add messages displayed on gui to hero methods

--- game/src/player/Hero.java
+++ game/src/player/Hero.java
@@ -42,11 +42,14 @@ public class Hero extends Character{
 			this.equippedItem = item;
 			item.equipped = true;
 			this.inventory.remove(item);
-			
+			String message = "You equip your " + item.getName();
 			if (temp != null) { //if their was an item previously equipped 
 				temp.equipped = false;
 				this.inventory.add(temp);
+				message += "and put your" + temp.getName() + " back into your inventory";
 			}
+			message += ".";
+			GuiGameWindow.GuiGameDisplay(message);
 		} else {
 			GuiGameWindow.GuiGameDisplay("This item is not equipable.");
 		}
@@ -55,12 +58,12 @@ public class Hero extends Character{
 	public void throwItem(Item item) {
 		if (this.inventory.contains(item)) { //if the designated item is in inventory we get rid of it
 			this.inventory.remove(item);
-		}
-		else if (this.equippedItem == item) { //if the designated item is equipped we either get rid of it or use it if throwable
+		} else if (this.equippedItem == item) { //if the designated item is equipped we either get rid of it or use it if throwable
 			this.equippedItem = null;
 			if (item.isThrowable()) {
 				//do item action
 			}
+			GuiGameWindow.GuiGameDisplay("You throw your " + item.getName() + ".");
 		}
 		else {
 			System.out.println("The item does not exist");
