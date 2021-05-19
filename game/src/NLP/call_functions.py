0aeb0f0a28c0db885704d4d26c71f02726ef5ce0
Hugo's Laptop <hugocarbiener@gmail.com>
2021-05-19 17:50:53 +0200

    Update NLP
    Changes made by Quentin

--- game/src/NLP/call_functions.py
+++ game/src/NLP/call_functions.py
@@ -3,57 +3,12 @@ import cleaning_and_parsing as cap
 import nltk
 from nltk.corpus import wordnet
 
-"""
-Sorties possibles :
-déplacement:
-    forward
-    backward
-    backtrack
-    moveTo
-attaque
-voir équipement
-utiliser objet
-
-data:
-ARMES
-Pair.with("Club", 1),
-Pair.with("Knife", 2),
-Pair.with("Dagger", 3),
-Pair.with("Bow", 4),
-Pair.with("Sword", 5),
-Pair.with("Axe", 6),
-Pair.with("Whip", 7),
-Pair.with("Claymore", 8),
-Pair.with("Scythe", 9),
-Pair.with("Magic AK47", 10)));
-
-MONSTRES
-
-Pair.with("Zombie", 1),
-Pair.with("Skeleton", 2),
-Pair.with("Ghost", 3),
-Pair.with("Undead", 4),
-Pair.with("Wolf", 5),
-Pair.with("Giant spider", 6),
-Pair.with("Slime", 7),
-Pair.with("Ghoul", 8),
-Pair.with("Necromancer", 9),
-Pair.with("Dragon", 10)));
-- move to location
-- attack monster with weapon
-- take item
-- throw item
-- equip item
-- look
-- check map/inventory
-"""
 # on renverra un tableau avec le verbe correspondant à l'action
 # ainsi que les éventuels arguments
 result = []
 
 items = ["club", "knife", "dagger", "bow", "sword", "axe", "whip", "claymore", "scythe", "katana"]
 monsters = ["zombie", "skeleton", "ghost", "undead", "wolf", "spider", "slime", "ghoul", "necromancer", "dragon"]
-#ATTENTION : on renverra la liste suivante : verbe, cible du verbe, moyen 
 
 #on commnence par les verbes
 # on considère qu'il n'y a qu'un verbe dans la phrase, on le récupère dans la liste
@@ -68,7 +23,7 @@ if verb == "":
     print("ERROR")
 else:
 
-    # verbes de référence : move, attack, use, look
+    # verbes de référence : les premiers de chaque liste
     # ces verbes servent à classifier le verbe donné par l'utilisateur
     # dans une des actions possibles correspondantes
 
@@ -76,12 +31,13 @@ else:
         ["move", "go", "run"], 
         ["attack", "kill", "slaughter"], 
         ["look", "see", "glance"], 
-        ["take", "pick", "choose"], 
+        ["take", "pick", "choose", "grab", "put"], 
         ["throw", "launch", "toss"], 
         ["equip", "gear", "prepare"],
         ["check", "verify", "study"]
     ]
     
+    # construction de la liste de synonymes de verbes
     syn_vbs = []
     for list_vbs in ref_vbs:
         tmp = []
@@ -90,6 +46,7 @@ else:
                for lemma in words.lemmas():
                     tmp.append(lemma.name())
         syn_vbs.append(tmp)
+    # construction de la liste de synonymes des items
     syn_items = []
     for item in items:
         tmp = []
@@ -97,6 +54,7 @@ else:
             for lemma in w.lemmas():
                 tmp.append(lemma.name())
         syn_items.append(tmp)
+    # construction de la liste de synonymes des monstres
     syn_monsters = []
     for monster in monsters:
         tmp = []
@@ -105,8 +63,7 @@ else:
                 tmp.append(lemma.name())
         syn_monsters.append(tmp)
                   
-        
-    # on cherche dans les synonymes des mots de référence
+    # on cherche dans les synonymes des mots de référence le mot nettoyé de l'entrée utilisateur
     matched = False
     for i in range(len(syn_vbs)):
         if verb in syn_vbs[i]:
